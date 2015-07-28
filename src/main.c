2865d94d79f7aaac32d7d131a3e9b61570b7d9e8
Kirby <kirby@pebble.com>
2015-07-28 14:41:22 -0700

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,66 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_text_layer;
+
+static void update_time() {
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+ 
+  static char buffer[] = "00:00";
+
+  strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+ 
+  text_layer_set_text(s_text_layer, buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void set_background_and_text_color(int color) {
+  GColor background_color = GColorFromHEX(color);
+  window_set_background_color(s_main_window, background_color);
+  text_layer_set_text_color(s_text_layer, gcolor_legible_over(background_color));
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_text_layer = text_layer_create(GRect(0, 55, bounds.size.w, 100));
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_LECO_38_BOLD_NUMBERS));
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(s_text_layer, GColorClear);
+
+  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+
+  update_time();
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(s_text_layer);
+}
+
+static void init() {
+  s_main_window = window_create();
+
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+
+  window_stack_push(s_main_window, true);
+
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main() {
+  init();
+  app_event_loop();
+  deinit();
+}
