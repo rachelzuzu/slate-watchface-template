9cecddc934b7c21401e34e94da677480c89e26d2
Kirby <kirby@pebble.com>
2015-07-28 20:01:05 -0700

    moar stuff

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,17 @@
+Pebble.addEventListener('ready', function() {
+  console.log('PebbleKit JS ready!');
+});
+
+Pebble.addEventListener('showConfiguration', function() {
+  var url = 'https://kirby.ngrok.io';
+
+  console.log('Showing configuration page: ' + url);
+
+  Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+
+  console.log('Configuration page returned: ' + JSON.stringify(configData));
+});
